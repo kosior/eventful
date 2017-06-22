2c4e516a2d2c0f8192c024abbb1c72cc44a49eb2
kosior <ksrk@protonmail.com>
2017-06-22 19:20:39 +0200

    added messages for user

--- eventful/events/views.py
+++ eventful/events/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.urls import reverse, reverse_lazy
 from django.utils import timezone
 from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
@@ -10,7 +11,7 @@ class EventDetail(DetailView):
     model = Event
 
     def get_object(self, **kwargs):
-        event = super(EventDetail, self).get_object(**kwargs)
+        event = super().get_object(**kwargs)
         event.views += 1
         event.save()
         return event
@@ -20,21 +21,35 @@ class EventActionMixin:
     form_class = CreateEventForm
     template_name = 'events/event_create_update.html'
 
+    @property
+    def success_msg(self):
+        return NotImplemented
+
+    def form_valid(self, form):
+        messages.success(self.request, self.success_msg, extra_tags='Success')
+        return super().form_valid(form)
+
     def get_success_url(self):
         return reverse('events:detail', args=(self.object.pk, ))
 
 
 class EventCreate(EventActionMixin, CreateView):
     model = Event
+    success_msg = 'Event created.'
+
+
+class EventUpdate(EventActionMixin, UpdateView):
+    model = Event
+    success_msg = 'Event updated.'
 
 
 class EventDelete(DeleteView):
     model = Event
     success_url = reverse_lazy('index')
 
-
-class EventUpdate(EventActionMixin, UpdateView):
-    model = Event
+    def delete(self, request, *args, **kwargs):
+        messages.success(request, 'Event deleted.', extra_tags='Success')
+        return super().delete(request, *args, **kwargs)
 
 
 class EventListView(ListView):
