c6b68391299bf3b8cc8b0da46ac23cecdff3bd92
kosior <ksrk@protonmail.com>
2017-06-29 22:14:21 +0200

    added email field to UserProfileForm, and added change password button to profile_detail template

--- eventful/userprofiles/forms.py
+++ eventful/userprofiles/forms.py
@@ -6,19 +6,22 @@ from userprofiles.models import UserProfile
 class UserProfileForm(forms.ModelForm):
     first_name = forms.CharField(label='First name', max_length=30)
     last_name = forms.CharField(label='Last name', max_length=30)
+    email = forms.EmailField(label='Email')
     website = forms.URLField(label='Website')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['first_name'].initial = self.instance.user.first_name
         self.fields['last_name'].initial = self.instance.user.last_name
+        self.fields['email'].initial = self.instance.user.email
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
         self.instance.user.first_name = self.cleaned_data.get('first_name')
         self.instance.user.last_name = self.cleaned_data.get('last_name')
+        self.instance.user.email = self.cleaned_data.get('email')
         self.instance.user.save()
 
     class Meta:
         model = UserProfile
-        fields = ('first_name', 'last_name', 'website')
+        fields = ('first_name', 'last_name', 'email', 'website')
