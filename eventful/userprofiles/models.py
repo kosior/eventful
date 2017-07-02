b0ce9e8c5d857eef6715992a171c7af331c4fccc
kosior <ksrk@protonmail.com>
2017-07-02 22:51:35 +0200

    added timezone field

--- eventful/userprofiles/models.py
+++ eventful/userprofiles/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class UserProfile(models.Model):
     user = models.OneToOneField(User, related_name='profile', on_delete=models.CASCADE, editable=False)
     website = models.URLField(blank=True)
+    timezone = models.CharField(max_length=35, blank=True)
 
     def __str__(self):
         return self.user.username
