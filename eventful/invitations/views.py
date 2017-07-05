51be27f987291db66991e4864f7cd95013677cd5
kosior <ksrk@protonmail.com>
2017-07-05 17:22:26 +0200

    refactored SendFriendRequest view to use custom model manager

--- eventful/invitations/views.py
+++ eventful/invitations/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.db import IntegrityError
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from django.utils.decorators import method_decorator
@@ -14,10 +13,6 @@ class SendFriendRequest(View):
     http_method_names = [u'post']
 
     def post(self, request):
-        to_user = get_object_or_404(User, pk=request.POST.get('to_user'))
-        try:
-            FriendInvitation.objects.create(inviter=request.user, to_user=to_user)
-        except IntegrityError:
-            return JsonResponse({'created': False})
-        else:
-            return JsonResponse({'created': True})
+        to_user = get_object_or_404(User, pk=request.POST.get('to_user_pk'))
+        created = FriendInvitation.objects.send_invitation(from_user=request.user, to_user=to_user)
+        return JsonResponse({'created': created})
