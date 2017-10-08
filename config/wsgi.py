bc939bcea4e13a4896bd51c0f520117a33c543b6
kosior <ksrk@protonmail.com>
2017-10-08 15:57:57 +0200

    set up environment variables; add project dir to path

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,16 +1,13 @@
-"""
-WSGI config for eventful project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
+
+app_path = os.path.dirname(os.path.abspath(__file__)).replace('/config', '')
+sys.path.append(os.path.join(app_path, 'eventful'))
+
+os.environ.setdefault('DJANGO_READ_DOT_ENV_FILE', 'True')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
 
 application = get_wsgi_application()
