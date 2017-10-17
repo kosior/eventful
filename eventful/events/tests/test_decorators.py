349384d22772f89299ba3e67308195499aa23e94
kosior <ksrk@protonmail.com>
2017-10-17 18:54:30 +0200

    add 2 tests for events/decorators.py

---
+++ eventful/events/tests/test_decorators.py
@@ -0,0 +1,29 @@
+from django.core.exceptions import PermissionDenied
+from django.urls import reverse
+from pytest import raises
+
+from .factories import EventFactory, UserFactory
+from ..decorators import user_is_event_author
+
+
+@user_is_event_author
+def fn_to_test(request, *args, **kwargs):
+    return True
+
+
+def test_user_is_event_author_when_is_not_creator(rf):
+    event = EventFactory()
+    user = UserFactory()
+    request = rf.get(reverse('event:detail', args=(event.pk,)))
+    request.user = user
+
+    with raises(PermissionDenied):
+        fn_to_test(request, pk=event.pk)
+
+
+def test_user_is_event_author_when_is_creator(rf):
+    event = EventFactory()
+    user = event.created_by
+    request = rf.get(reverse('event:detail', args=(event.pk,)))
+    request.user = user
+    assert fn_to_test(request, pk=event.pk)
